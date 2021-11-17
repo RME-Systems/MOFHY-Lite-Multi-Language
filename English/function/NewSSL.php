d47517135e3e5acded422d023c1bc7d64c82570d
Greenreader9 <75644395+greenreader9@users.noreply.github.com>
2021-11-17 14:42:48 -0600

    Update NewSSL.php
    Fixed Spelling

--- English/function/NewSSL.php
+++ English/function/NewSSL.php
@@ -117,7 +117,7 @@ if(isset($_POST['submit'])){
 										  <button class="close" data-dismiss="alert" type="button" aria-label="Close">
 										    <span aria-hidden="true">&times;</span>
 										  </button>
-										  Something wemt'."'".' <b>weong!</b>
+										  Something went <b>wrong!</b>
 										</div>';
 			header('location: ../newssl.php');
 		}
